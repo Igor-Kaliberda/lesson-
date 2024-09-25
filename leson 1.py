1dcf7294a8879542e04e2a68ec2f2a5e07303e1f
jack2 <jfaka@i.ua>
2024-09-25 20:50:26 +0300

    commit

---
+++ leson 1.py
@@ -0,0 +1,17 @@
+# print("hello world!")
+#
+# print("hello world!")
+# print()
+# print('Test')
+
+# print("ww\nwww")
+# print("ssds\n\tdss")
+# print("ww\\n\\t\"wwwww\"")
+# print("dead\tmazai")
+# print("hello world", end="")
+# print('test')
+# print("hello got", "rttt", "rext" )
+print("hello www", "vasya", "sety", sep=", ", end=" - ")
+print('test')
+
+print("ddd vokl")
