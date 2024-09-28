8459ddd9a6ae54e451fd1ec581abe3671780dc34
jack2 <jfaka@i.ua>
2024-09-28 13:33:52 +0300

    changest

--- leson 1.py
+++ leson 1.py
@@ -3,6 +3,7 @@
 # print("hello world!")
 # print()
 # print('Test')
+from operator import truediv
 
 # print("ww\nwww")
 # print("ssds\n\tdss")
@@ -11,7 +12,59 @@
 # print("hello world", end="")
 # print('test')
 # print("hello got", "rttt", "rext" )
-print("hello www", "vasya", "sety", sep=", ", end=" - ")
-print('test')
+# print("hello www", "vasya", "sety", sep=", ", end=" - ")
+# print('test')
 
-print("ddd vokl")
+# number = 10 + 1
+# print(number)
+# print(type(number))
+#
+# number = 20.6
+# print(number)
+# print(type(number))
+#
+# number = "rrrr"
+# print(number)
+# print(type(number))
+#
+# number = True
+# print(number)
+# print(type(number))
+
+
+# userName = "wwwww"
+# print(userName)
+# user_name = "wwwww"
+# print(user_name)
+#
+# number1 = 10
+# print(number1)
+# print(number := 10) моржовый оператор
+
+# print(2 + 3)
+# print(2 - 3)
+# print(2 * 3)
+# print(2 / 3)
+# print(2 ** 3)  основа ** показник (зведеня в ступінь)
+# print(2 // 3)  ціла частина від розділу
+# print(2 % 3) залишок від ділення
+
+
+# number = 356
+# n1 = number // 100
+# n2 = number % 100 // 10
+# n2_v2 =  number // 10 % 10
+# n3 = number % 10
+#
+# print(n1)
+# print(n2)
+# print(n2_v2)
+# print(n3)
+
+# first_number = input("Enter first number ")
+# second_number = input("enter second number " )
+# result = first_number + second_number
+# print(result) # resultat
+
+
+
