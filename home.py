04135b8e146311b01586ebd0ef8269298b2d8593
jack2 <jfaka@i.ua>
2024-09-29 17:29:24 +0300

    home work

--- home.py
+++ home.py
@@ -1,1 +1,23 @@
-print("Hello World")
+# print("Hello World")
+
+number = 4652
+
+n1 = number // 1000
+n2 = number // 100 % 10
+n3 = number // 10 % 10
+n4 = number % 10
+
+print(n1)
+print(n2)
+print(n3)
+print(n4)
+
+# 56893
+I_n = int(input("Enter 1 number "))
+II_n = int(input("Enter 2 number "))
+III_n = int(input("Enter 3 number "))
+IV_n = int(input("Enter 4 number " ))
+V_n = int(input("Tnter 5 number " ))
+
+result = I_n * 1 + II_n * 10 + III_n * 100 + IV_n * 1000 + V_n * 10000
+print(result) # resultat
