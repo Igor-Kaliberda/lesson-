ed78b8d281e82196207ff25bc179f305da54c1f9
jack2 <jfaka@i.ua>
2024-09-29 18:47:13 +0300

    home work

--- leson 1.py
+++ leson 1.py
@@ -3,6 +3,7 @@
 # print("hello world!")
 # print()
 # print('Test')
+from pprint import PrettyPrinter
 
 # print("ww\nwww")
 # print("ssds\n\tdss")
@@ -11,7 +12,78 @@
 # print("hello world", end="")
 # print('test')
 # print("hello got", "rttt", "rext" )
-print("hello www", "vasya", "sety", sep=", ", end=" - ")
-print('test')
+# print("hello www", "vasya", "sety", sep=", ", end=" - ")
+# print('test')
 
-print("ddd vokl")
+# number = 10 + 1
+# print(number)
+# print(type(number))
+#
+# number = 20.6
+# print(number)
+# print(type(number))
+#
+# number = "rrrr"
+# print(number)
+# print(type(number))
+#
+# number = True
+# print(number)
+# print(type(number))
+
+
+# userName = "wwwww"
+# print(userName)
+# user_name = "wwwww"
+# print(user_name)
+#
+# number1 = 10
+# print(number1)
+# print(number := 10) моржовый оператор
+
+# print(2 + 3)
+# print(2 - 3)
+# print(2 * 3)
+# print(2 / 3)
+# print(2 ** 3)  основа ** показник (зведеня в ступінь)
+# print(2 // 3)  ціла частина від розділу
+# print(2 % 3) залишок від ділення
+
+
+# number = 356
+# n1 = number // 100
+# n2 = number % 100 // 10
+# n2_v2 =  number // 10 % 10
+# n3 = number % 10
+#
+# print(n1)
+# print(n2)
+# print(n2_v2)
+# print(n3)
+#
+# # first_number = input("Enter first number ")
+# # second_number = input("enter second number " )
+# # result = first_number + second_number
+# # print(result) # resultat
+
+number = 4652
+
+n1 = number // 1000
+n2 = number // 100 % 10
+n3 = number // 10 % 10
+n4 = number % 10
+
+print(n1)
+print(n2)
+print(n3)
+print(n4)
+
+# 56893
+I_n = int(input("Enter 1 number "))
+II_n = int(input("Enter 2 number "))
+III_n = int(input("Enter 3 number "))
+IV_n = int(input("Enter 4 number " ))
+V_n = int(input("Tnter 5 number " ))
+
+result = I_n * 1 + II_n * 10 + III_n * 100 + IV_n * 1000 + V_n * 10000
+print(result) # resultat
