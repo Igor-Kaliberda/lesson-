c61f00b88d7834fcd7446dab08a7fe864d56568b
jack2 <jfaka@i.ua>
2024-10-05 18:39:59 +0300

    home work

---
+++ Domashka.py
@@ -0,0 +1,23 @@
+kalk = int(input("1. +\n2. -\n3. *\n4. /"))
+
+if kalk == 1:
+    n1 = int(input("Enter 1 number "))
+    n2 = int(input("Enter 2 number "))
+    n3 = n1 + n2
+    print(n3)
+elif kalk == 2:
+    n1 = int(input("Enter 1 number "))
+    n2 = int(input("Enter 2 number "))
+    n3 = n1 - n2
+    print(n3)
+elif kalk == 3:
+    n1 = int(input("Enter 1 number "))
+    n2 = int(input("Enter 2 number "))
+    n3 = n1 * n2
+    print(n3)
+elif kalk == 4:
+    n1 = int(input("Enter 1 number "))
+    n2 = int(input("Enter 2 number "))
+    n3 = n1 / n2
+    print(n3)
+
