c5e6421cd07b3b07792f9eac8aeac2c673acbb16
jack2 <jfaka@i.ua>
2024-10-07 21:37:33 +0300

    home work

---
+++ domashka2.py
@@ -0,0 +1,17 @@
+
+
+import random
+numbers = []
+i = [1, 2, 3, 4, 5, 6, 7, 8]
+for i in range(random.randint(1, 3)):
+    numbers.append(random.randint(1, 8))
+print(numbers)
+
+
+import random
+numbers = []
+i = [1, 1, 2, 6]
+for i in range(random.randint(1, 4)):
+    numbers.append(random.randint(1, 4))
+
+print(numbers)
