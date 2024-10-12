d16b63c5c3b57bb8a9d0b608e8cb88e86ba8f434
jack2 <jfaka@i.ua>
2024-10-12 17:51:01 +0300

    home work

--- dom.py
+++ dom.py
@@ -1,9 +1,37 @@
-numbers = [1, 3, 6, 8]
-def Sumlist(numbers):
-    suma = 0
-    for i in range(len(numbers)):
-        suma += numbers[i]
-        return suma
 
-resultat = Sumlist(numbers)
-print(resultat)
+
+while True:
+    kalk = int(input("1. +\n2. -\n3. *\n4. /\n5. exit "))
+
+    match kalk:
+            case 1:
+                n1 = int(input("Enter 1 number "))
+                n2 = int(input("Enter 2 number "))
+                result = n1 + n2
+                print(result)
+                continue
+            case 2:
+                n1 = int(input("Enter 1 number "))
+                n2 = int(input("Enter 2 number "))
+                result2 = n1 - n2
+                print(result2)
+                continue
+            case 3:
+                n1 = int(input("Enter 1 number "))
+                n2 = int(input("Enter 2 number "))
+                result3 = n1 * n2
+                print(result3)
+                continue
+            case 4:
+                n1 = int(input("Enter 1 number "))
+                n2 = int(input("Enter 2 number "))
+                result4 = n1 / n2
+                print(result4)
+            case 5:
+                end = int(input("exit"))
+                print(end)
+                break
+
+
+
+
