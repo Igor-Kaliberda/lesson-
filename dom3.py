245c208d2c769ce40c801cb584207bcf262279a0
jack2 <jfaka@i.ua>
2024-10-13 14:13:48 +0300

    home work

---
+++ dom3.py
@@ -0,0 +1,27 @@
+import string
+
+# my_list = "Pithon Communiti"
+# my_list =  my_list.replace(" ", '')
+# print(my_list)
+
+
+# my_string = "i like pithon community"
+# my_string =  my_string.title()
+# for my_string2 in my_string:
+#  my_string2 = my_string.replace(" ", '')
+#  print(my_string2, end= " ")
+
+my_rist = "Should, I. subscribe? Yes"
+for rist in my_rist:
+ rist = my_rist.replace(',.?!', "") + my_rist.title()
+print(rist)
+
+
+
+
+
+# special_string = "spe@#$ci87al\*&"
+# a = special_string.replace('@#$\*&', '')
+# print(a)
+
+
