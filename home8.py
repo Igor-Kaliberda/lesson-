a14bfdc79dca497688583ef0ee33136c935f74ca
jack2 <jfaka@i.ua>
2024-10-24 16:42:55 +0300

    home work

---
+++ home8.py
@@ -0,0 +1,19 @@
+text = """When I was One I had just begun When I was Two I was nearly new"""
+words = ['i', 'was', 'three', 'near']
+
+def Text_words(text, words):
+
+    text = text.lower()
+    word_list = text.split()
+    word_count = {word: 0 for word in words}
+    for word in words:
+        word_count[word] = word_list.count(word)
+
+    return word_count
+
+
+
+
+
+
+print(Text_words(text, words))
