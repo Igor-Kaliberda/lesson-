09841a501b68b13e03f0f5fbfa31393a38a15d75
jack2 <jfaka@i.ua>
2024-11-09 12:53:55 +0200

    home work

--- home5.py
+++ home5.py
@@ -1,10 +1,60 @@
-numbers1 = [1, 2, 3, 4]
-numbers2 = [9, 9, 9,]
-def add_one(digits):
-    number1 = int(''.join(map(str, digits)))
-    number1 += 1
-    return [int(digit) for digit in str(number1)]
+class Counter:
+    def __init__(self, current=1, min_value=0, max_value=10):
+        self.current = current
+        self.min_value = min_value
+        self.max_value = max_value
 
+    def set_current(self, start):
+        if start < self.min_value or start > self.max_value:
+            raise ValueError("Initial value out of bounds")
+        self.current = start
 
-print(add_one(numbers1))
-print(add_one(numbers2))
+    def set_max(self, max_max):
+        if max_max < self.min_value:
+            raise ValueError("Maximum value cannot be less than the minimum value")
+        self.max_value = max_max
+        if self.current > self.max_value:
+            self.current = self.max_value
+
+    def set_min(self, min_min):
+        if min_min > self.max_value:
+            raise ValueError("Minimum value cannot be greater than the maximum value")
+        self.min_value = min_min
+        if self.current < self.min_value:
+            self.current = self.min_value
+
+    def step_up(self):
+        if self.current >= self.max_value:
+            raise ValueError("Достигнут максимум")
+        self.current += 1
+
+    def step_down(self):
+        if self.current <= self.min_value:
+            raise ValueError("Достигнут минимум")
+        self.current -= 1
+
+    def get_current(self):
+        return self.current
+
+counter = Counter()
+counter.set_current(7)
+counter.step_up()
+counter.step_up()
+counter.step_up()
+assert counter.get_current() == 10, 'Test1'
+try:
+    counter.step_up()
+except ValueError as e:
+    print(e)
+assert counter.get_current() == 10, 'Test2'
+
+counter.set_min(7)
+counter.step_down()
+counter.step_down()
+counter.step_down()
+assert counter.get_current() == 7, 'Test3'
+try:
+    counter.step_down()
+except ValueError as e:
+    print(e)
+assert counter.get_current() == 7, 'Test4'
