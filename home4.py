1a007ec3a7f52123ae268db658f4a287a1376f64
jack2 <jfaka@i.ua>
2024-11-09 12:20:35 +0200

    home work

--- home4.py
+++ home4.py
@@ -1,13 +1,26 @@
-def hello(name: str, age: int) -> str:
-    if not isinstance(name, str):
-        raise ValueError("Name must be a string")
-
-    if not isinstance(age, int) or age <= 0:
-        raise ValueError("Age must be a positive integer")
-
-    return f"Hi. My name is {name} and I am {age} years old."
-
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
 
 
-print(hello("Stiv", 30))
-print(hello("Hella", 25))
+def prime_generator(end):
+    num = 2
+    while num <= end:
+        if is_prime(num):
+            yield num
+        num += 1
+
+
+# Перевірка
+from inspect import isgenerator
+
+gen = prime_generator(1)
+assert isgenerator(gen) == True, 'Test0'
+assert list(prime_generator(10)) == [2, 3, 5, 7], 'Test1'
+assert list(prime_generator(15)) == [2, 3, 5, 7, 11, 13], 'Test2'
+assert list(prime_generator(29)) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29], 'Test3'
+print('Test passed!')
