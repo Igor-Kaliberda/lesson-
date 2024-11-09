66282caff5aefdc7609dd97539629f72b70cd659
jack2 <jfaka@i.ua>
2024-11-09 13:01:28 +0200

    home work

--- home6.py
+++ home6.py
@@ -1,12 +1,55 @@
-import re
-spisok = "A man, a plan, a canal, Panama"
-spisok2 = "Hello, World!"
-spisok3 = "a."
+class StudentLimitException(Exception):
 
-def is_palindrome(s):
-    cleaned = re.sub(r'[^A-Za-z0-9]', '', s).lower()
-    return cleaned == cleaned[::-1]
+    def __init__(self, message="limit is reached"):
+        self.message = message
+        super().__init__(self.message)
 
-print(is_palindrome(spisok))
-print(is_palindrome(spisok2))
-print(is_palindrome(spisok3))
+
+class Student:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Group:
+    def __init__(self, name):
+        self.name = name
+        self.students = []
+
+    def add_student(self, student):
+        if len(self.students) >= 10:
+            raise StudentLimitException()
+        self.students.append(student)
+
+    def remove_student(self, student):
+        self.students.remove(student)
+
+    def __str__(self):
+        return f"Grup {self.name}: " + ", ".join(str(student) for student in self.students)
+
+students = [
+    Student("Andrii", "Shevchenko"),
+    Student("Olia", "Kravchuk"),
+    Student("Petro", "Ivanov"),
+    Student("Iryna", "Kovalenko"),
+    Student("Mykola", "Petrenko"),
+    Student("Viktoria", "Saienko"),
+    Student("Anton", "Bondarenko"),
+    Student("Nadiia", "Tkachenko"),
+    Student("Oleksii", "Pavlenko"),
+    Student("Alina", "Dovzhenko"),
+    Student("Yurii", "Shapoval")
+]
+
+group = Group("ІТ")
+
+try:
+    for student in students:
+        group.add_student(student)
+except StudentLimitException as e:
+    print(e)
+
+print(group)
